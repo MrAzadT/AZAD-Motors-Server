27e1362f93943af5f3a835aeca700bdbb81d6c3e
azadtamal <mrazadt@gmail.com>
2021-11-11 18:18:33 +0600

    mongo

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikemc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikemc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,11 +19,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db("carsell");
-    const carsDataCollection = database.collection("car");
+    const database = client.db("carSell");
+    const carsCollection = database.collection("cars");
 
-    app.get("/myData", async (req, res) => {
-      const cursor = carsDataCollection.find({});
+    app.get("/cars", async (req, res) => {
+      const cursor = carsCollection.find({});
       const data = await cursor.toArray();
       res.send(data);
     });
